b612cd2b352f51d0b56a4f253c183209e9f93cae
Federex  A  Potolin <potolin.federex@gmail.com>
2025-04-24 19:29:23 +0800

    first commit

--- code.py
+++ code.py
@@ -31,7 +31,7 @@ y_le = LabelEncoder()
 y = y_le.fit_transform(y)
 
 # Split dataset
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Models
 models = {
